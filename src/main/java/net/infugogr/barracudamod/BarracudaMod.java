a65bb8ef6da8e3f54af71979c328fdade5b86c0a
Hijedepytto <tolik_ebolik_2019@mail.ru>
2024-03-24 10:43:21 +1100

    Added new item (Aquatic_Dust)

--- src/main/java/net/infugogr/barracudamod/BarracudaMod.java
+++ src/main/java/net/infugogr/barracudamod/BarracudaMod.java
@@ -1,16 +1,17 @@
 package net.infugogr.barracudamod;
 
 import net.fabricmc.api.ModInitializer;
+import net.infugogr.barracudamod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class BarracudaMod implements ModInitializer {
 
-	public static final String MOD_ID = "baracudamod";
+	public static final String MOD_ID = "barracudamod";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
-		LOGGER.info("Hello Fabric world!");
+		ModItems.registerModItems();
 	}
 }
---
+++ src/main/java/net/infugogr/barracudamod/item/ModItems.java
@@ -0,0 +1,20 @@
+package net.infugogr.barracudamod.item;
+
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.infugogr.barracudamod.BarracudaMod;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+
+public class ModItems {
+    public static final Item AQUATIC_DUST = registerItem("aquatic_dust",
+            new Item(new FabricItemSettings().group(ItemGroup.MISC)));
+
+    private static Item registerItem(String name, Item item){
+        return Registry.register(Registry.ITEM, new Identifier(BarracudaMod.MOD_ID, name), item);
+    }
+    public static void registerModItems(){
+        BarracudaMod.LOGGER.info("Registering Mod Items for" + BarracudaMod.MOD_ID);
+    }
+}
